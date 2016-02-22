2c3f765845a015fc1a5238e846c4887b17cde53a
cjbirch <birch@atm.ox.ac.uk>
2016-02-22 18:25:47 +0000

    Added detailed view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
+# ^ -- "the beginning"
+# post/ -- what comes next in the URL
+# (?P<pk>[0-9]+) -- Django will take everything that is typed here and 
+# define a variable pk (primary key). [0-9] also tells us that it can 
+# only be an integer, not a letter (between 0 and 9). 
+# + means that there needs to be one or more digits there.  
+# So http://127.0.0.1:8000/post/1234567890/ but not /post//.
+# $ -- "the end"
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create views here.
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+    # http://127.0.0.1:8000/post/pk/ will retrun the full text of a post
